c25090a149f529735ec1d39c5bf2329657b38411
A Harrod <100361757+alha89@users.noreply.github.com>
2024-12-11 20:16:22 +0000

    Add files via upload

---
+++ Additional_scripts/38_MEME_SEA_motifs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=24
+#SBATCH --time=28:00:00
+#SBATCH --partition=compute
+#SBATCH --mem-per-cpu=8000
+
+cd ..
+
+# Download motif_databases folder and software source code from: https://meme-suite.org/meme/doc/download.html
+# Installation guide: https://meme-suite.org/meme/doc/install.html?man_type=web
+
+# tested with MEME/5.4.1-gompi-2021b
+
+# Also requires bedtools package (tested with version 2.29.2)
+
+# Analysis of CENPB enriched motifs from fig3H
+
+mkdir -p MEME_SEA_motifs
+
+# unique peaks
+
+
+bedtools getfasta -fi genomes/GCA_009914755.3_genomic_chr.fna -bed unique_reads/MACS2_peaks/overlap_2/CEN_peaks/WT_CENPB_vs_IgG_overlap_cen_peaks_merge.bed \
+ -fo unique_reads/MACS2_peaks/overlap_2/CEN_peaks/WT_CENPB_vs_IgG_overlap_cen_peaks_merge.fa
+
+
+sea --p unique_reads/MACS2_peaks/overlap_2/CEN_peaks/WT_CENPB_vs_IgG_overlap_cen_peaks_merge.fa \
+ --m motif_databases/HOCOMOCO/H12CORE_meme_format.meme --o MEME_SEA_motifs/WT_CENPB_peaks_MEME_HUMAN_H12core_full_shuffled
+
+# k-mers
+
+sea --p kmer_centromere/enriched_kmers/overlap_2/WT_CENPB_overlap_2.fa \
+ --m motif_databases/HOCOMOCO/H12CORE_meme_format.meme --o MEME_SEA_motifsWT_CENPB_k-mers_MEME_HUMAN_H12core_full_shuffled
+
---
+++ Additional_scripts/39_reldist_k-mers.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:00:00
+#SBATCH --partition=compute
+#SBATCH --mem-per-cpu=8000
+
+cd ..
+
+
+# Also requires bedtools package (tested with version 2.29.2)
+
+# Specific relative distance calculations used to prepare supplementary figure S10C-D
+# _summary.txt file outputs were used to calculate fractions (count/total) and cumulative fractions for each relative distance (reldist) and then plot in Graphpad prism
+
+# This requires rerunning 26_enrich_kmer_fa and 27_map_kmers scripts but on the output files from 29_kmer_specific_groups script
+
+mkdir -p kmer_centromere/enriched_kmers/overlap_2/reldist
+
+# convert bam files to bed file
+
+bedtools bamtobed -i kmer_centromere/enriched_kmers/overlap_2/PBRM1_specific_kmers_fc2.bam > kmer_centromere/enriched_kmers/overlap_2/PBRM1_specific_kmers_fc2.bed
+
+bedtools bamtobed -i kmer_centromere/enriched_kmers/overlap_2/PBRM1_non-specific_kmers_fc2.bam > kmer_centromere/enriched_kmers/overlap_2/PBRM1_non-specific_kmers_fc2.bed
+
+bedtools bamtobed -i kmer_centromere/enriched_kmers/overlap_2/KO_specific_kmers_fc2.bam > kmer_centromere/enriched_kmers/overlap_2/KO_specific_kmers_fc2.bed
+
+# Do the same as above with equivalent for histone PTM ChIP-seq GSE175752
+# (after following full k-mer analysis pipeline as before, but with exceptions found in Additional_scripts/Exceptions_for_ChIP-seq.txt)
+
+bedtools reldist -a kmer_centromere/enriched_kmers/overlap_2/PBRM1_specific_kmers_fc2.bed \
+ -b kmer_centromere/ChIP_seq/enriched_kmers/overlap_2/kmers_H3K4me3_fc2.bed \
+ > PBRM1_specific_reldist_kmers_H3K4me3_summary.txt
+
+bedtools reldist -a kmer_centromere/enriched_kmers/overlap_2/PBRM1_non-specific_kmers_fc2.bed \
+ -b kmer_centromere/ChIP_seq/enriched_kmers/overlap_2/kmers_H3K4me3_fc2.bed \
+ > PBRM1_non-specific_reldist_kmers_H3K4me3_summary.txt
+
+bedtools reldist -a kmer_centromere/enriched_kmers/overlap_2/KO_specific_kmers_fc2.bed \
+ -b kmer_centromere/ChIP_seq/enriched_kmers/overlap_2/kmers_H3K4me3_fc2.bed \
+ > KO_specific_reldist_kmers_H3K4me3_summary.txt
+
+
+bedtools reldist -a kmer_centromere/enriched_kmers/overlap_2/PBRM1_specific_kmers_fc2.bed \
+ -b kmer_centromere/ChIP_seq/enriched_kmers/overlap_2/kmers_H3K9me3_fc2.bed \
+ > PBRM1_specific_reldist_kmers_H3K9me3_summary.txt
+
+bedtools reldist -a kmer_centromere/enriched_kmers/overlap_2/PBRM1_non-specific_kmers_fc2.bed \
+ -b kmer_centromere/ChIP_seq/enriched_kmers/overlap_2/kmers_H3K9me3_fc2.bed \
+ > PBRM1_non-specific_reldist_kmers_H3K9me3_summary.txt
+
+bedtools reldist -a kmer_centromere/enriched_kmers/overlap_2/KO_specific_kmers_fc2.bed \
+ -b kmer_centromere/ChIP_seq/enriched_kmers/overlap_2/kmers_H3K9me3_fc2.bed \
+ > KO_specific_reldist_kmers_H3K9me3_summary.txt
+
+
+bedtools reldist -a kmer_centromere/enriched_kmers/overlap_2/PBRM1_specific_kmers_fc2.bed \
+ -b kmer_centromere/ChIP_seq/enriched_kmers/overlap_2/kmers_H3K9ac_fc2.bed \
+ > PBRM1_specific_reldist_kmers_H3K9ac_summary.txt
+
+bedtools reldist -a kmer_centromere/enriched_kmers/overlap_2/PBRM1_non-specific_kmers_fc2.bed \
+ -b kmer_centromere/ChIP_seq/enriched_kmers/overlap_2/kmers_H3K9ac_fc2.bed \
+ > PBRM1_non-specific_reldist_kmers_H3K9ac_summary.txt
+
+bedtools reldist -a kmer_centromere/enriched_kmers/overlap_2/KO_specific_kmers_fc2.bed \
+ -b kmer_centromere/ChIP_seq/enriched_kmers/overlap_2/kmers_H3K9ac_fc2.bed \
+ > KO_specific_reldist_kmers_H3K9ac_summary.txt
